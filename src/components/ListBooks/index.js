1c0ef2892175ccf576d64e85ee3037eff4bb885f
Caio Oliveira <caio.oliveira4@nextel.com.br>
2018-10-30 16:38:52 -0300

    added Select component

--- src/components/ListBooks/index.js
+++ src/components/ListBooks/index.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Select } from '../Select';
 
 class ListBooks extends Component {
-  render() {
-    const { children } = this.props;
+  renderBooks = () => {
+    const { books } = this.props;
     return (
-      <div className="list-books-content">
-        <div>
-          { children }
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+          <div className="book-shelf-changer">
+            <Select />
+          </div>
         </div>
+        <div className="book-title">To Kill a Mockingbird</div>
+        <div className="book-authors">Harper Lee</div>
       </div>
+    );
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {this.renderBooks()}
+      </Fragment>
     );
   }
 }
 
 ListBooks.propTypes = {
-  children: PropTypes.string,
+  books: PropTypes.array,
 };
 
 export default ListBooks;
--- src/containers/HomeView/index.js
+++ src/containers/HomeView/index.js
@@ -1,36 +1,27 @@
 import React, { Fragment } from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from '../../BooksAPI';
 import '../../App.css';
 import ListBooks from '../../components/ListBooks';
 import NavBar from '../../components/NavBar';
 import Button from '../../components/Button';
 
+
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
+    books: [],
   }
 
-  toggleSearch = () => {
-    const { showSearchPage } = this.state;
-    this.setState({
-      showSearchPage: !showSearchPage,
-    });
+  componentWillMount = () => {
+    BooksAPI.getAll().then(books => this.setState({ books }));
   }
 
   render() {
+    const { books } = this.state;
     return (
       <div className="app">
         <Fragment>
           <NavBar />
-          <ListBooks>
-            Hello World
-          </ListBooks>
+          <ListBooks books={books} />
           <Button path="/search" />
         </Fragment>
       </div>
