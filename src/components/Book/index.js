a2bc9c76619764f800c59a2f87583feba6c4cca3
Caio Oliveira <caio.oliveira4@nextel.com.br>
2018-11-08 18:13:33 -0200

    inserted select into book component

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -10,6 +10,16 @@ class Book extends Component {
     ));
   };
 
+  renderSelect = () => (
+    <select>
+      <option value="move" disabled>Move to...</option>
+      <option value="currentlyReading">Currently Reading</option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Read</option>
+      <option value="none">None</option>
+    </select>
+  );
+
   renderBooks = () => {
     const { title, authors, thumbnail } = this.props;
     return (
@@ -17,7 +27,7 @@ class Book extends Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${thumbnail}")` }} />
           <div className="book-shelf-changer">
-            <Select />
+            {this.renderSelect()}
           </div>
         </div>
         <div className="book-title">{title}</div>
